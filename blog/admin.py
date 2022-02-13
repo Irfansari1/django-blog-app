7734ba93839f4780d089ce354203dad6b92e194c
Irfansari1 <irfansari42@gmail.com>
2022-02-13 14:49:26 +0100

    users and posts added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Post, PostView, Comment
+
+admin.site.register(Category)
+admin.site.register(Post)
+#admin.site.register(Like)
+admin.site.register(PostView)
+admin.site.register(Comment)
---
+++ blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.db.models import fields
+from .models import Post, Category, Comment
+
+
+class PostForm(forms.ModelForm):
+    status = forms.ChoiceField(choices=Post.OPTIONS)
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(), empty_label="Select")
+
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'content',
+            'image',
+            'category',
+            'status',
+        )
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
