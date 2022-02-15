623fc6e9b48267827fc3adeadcf54b57f77d16b9
Irfansari1 <irfansari42@gmail.com>
2022-02-15 12:07:15 +0100

    heroku updates added

--- main/settings.py
+++ main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,15 +127,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+ os.path.join(BASE_DIR, 'static'),
+)
+
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media_root"
 
-
-
-""" STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-] """
 
 
 
@@ -147,4 +152,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+django_heroku.settings(locals())
